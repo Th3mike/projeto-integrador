d60da7c1fc6499edf116ec31914ccd77ce3a3fe9
th3mike <maarcio_lacerda@hotmail.com>
2021-03-15 10:49:50 -0300

    Footer e menu, base do index

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,28 @@
     <h1>teste</h1>
     <h1>teste</h1>
     <h1>teste</h1>
-    <h1>teste</h1><h1>teste</h1>
-    <h1>teste</h1><h1>teste</h1>
     <h1>teste</h1>
-    <h1>teste</h1><h1>teste</h1><h1>teste</h1>
-    <h1>teste</h1><h1>teste</h1><h1>teste</h1>
-    <h1>teste</h1><h1>teste</h1>
-    <h1>teste</h1><h1>teste</h1>
     <h1>teste</h1>
-    <h1>teste</h1><h1>teste</h1><h1>teste</h1><h1>teste</h1><h1>teste</h1><h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
+    <h1>teste</h1>
     <h1>teste</h1>
     <Footer />
   </div>
