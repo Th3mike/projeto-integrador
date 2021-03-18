a51f266278cd4827c3ed454794ca694f0b92e74b
th3mike <maarcio_lacerda@hotmail.com>
2021-03-18 10:21:59 -0300

    dbe2f58

---
+++ _nuxt/static/1616073702/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F%3Cprojeto-integrador%3E\u002F_nuxt\u002Fstatic\u002F1616073702",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002F%3Cprojeto-integrador%3E\u002F",assetsPath:"\u002F%3Cprojeto-integrador%3E\u002F_nuxt\u002F",cdnURL:a}}}}(null));
