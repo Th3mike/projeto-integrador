8b3ab1fa9f8af893f53f0d4c1003e22c77a00153
th3mike <maarcio_lacerda@hotmail.com>
2021-03-26 10:47:50 -0300

    test para auto deploy

--- dist/_nuxt/static/1616764424/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fprojeto-integrador\u002F_nuxt\u002Fstatic\u002F1616764424",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fprojeto-integrador\u002F",assetsPath:"\u002Fprojeto-integrador\u002F_nuxt\u002F",cdnURL:a}}}}(null));
