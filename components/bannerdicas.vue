c255e127e98cd207bc13ba8ff40a38fd85df1742
diegodmb97 <diegomhb@gmail.com>
2021-04-06 10:53:25 -0300

    Page Dicas

---
+++ components/bannerdicas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner px-6">
+
+  </div>
+</template>
+
+
+<style scoped>
+.banner{
+  width: 100%;
+  height: 400px;
+  background-color: #DECBB7;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
