e4a8b27ecc4de2a6cec7317f5f2940bef828b47d
th3mike <maarcio_lacerda@hotmail.com>
2021-04-07 18:45:00 -0300

    mudança no router, test 1

--- dist/_nuxt/static/1617807720/contato/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fprojeto-integrador\u002F_nuxt\u002Fstatic\u002F1617807720",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontato",config:{_app:{basePath:"\u002Fprojeto-integrador\u002F",assetsPath:"\u002Fprojeto-integrador\u002F_nuxt\u002F",cdnURL:a}}}}(null));
