b5a0d707bcc4669f6c6e2b49afefd192b0d41297
Marcos Vinícius Rodrigues de Souza <marcossouza.0ds@gmail.com>
2021-05-28 09:28:50 -0300

    Pronto

--- pages/produtos.vue
+++ pages/produtos.vue
@@ -7,6 +7,30 @@
 </template>
 
 <script>
+import Vue from 'vue';
+
+import Meta from 'vue-meta';
+
+Vue.use(Meta);
+
+
+export default {
+    name: 'Produtos',
+    metaInfo: {
+      title: 'Stacy | Seja bem vindo!',
+      // override the parent template and just use the above title only
+      titleTemplate: '%s ← Produtos |',
+      htmlAttrs: {
+      lang: 'en-US'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'description', content: 'Confira todos nossos produtos separados por categoria!' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  }
+  
+}
 </script>
 
 <style escoped>
